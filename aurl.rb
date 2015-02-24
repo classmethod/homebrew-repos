0e42d7f20486f98d7d95d9cc89ed5e42cc9155e2
Daisuke Miyamoto <miyamoto.daisuke@classmethod.jp>
2015-02-24 14:14:27 +0900

    release aurl 0.2.1

--- aurl.rb
+++ aurl.rb
@@ -2,9 +2,14 @@ require "formula"
 
 class Aurl < Formula
   homepage "https://github.com/classmethod-aws/aurl"
-  version '0.2'
-  url "https://github.com/classmethod-aws/aurl/releases/download/0.2/aurl_0.2_darwin_amd64.zip"
-  sha1 "d1e25b788d58fcbc6b9295940652692cb3722292"
+  version '0.2.1'
+  if Hardware.is_64_bit?
+    url "https://github.com/classmethod-aws/aurl/releases/download/0.2.1/aurl_0.2.1_darwin_amd64.zip"
+    sha1 "244f245b8a67ae744906c7ce9cb9d13647618085"
+  else
+    url "https://github.com/classmethod-aws/aurl/releases/download/0.2.1/aurl_0.2.1_darwin_386.zip"
+    sha1 "bc0643897b6807b42474381a0b882f8a665b6a1e"
+  end
 
   depends_on :arch => :intel
 
