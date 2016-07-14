55c7076752b835c7352320e1a923a3fb10438c0d
Daisuke Miyamoto <miyamoto.daisuke@classmethod.jp>
2016-07-15 03:05:16 +0900

    release aurl-1.0

--- aurl.rb
+++ aurl.rb
@@ -1,51 +1,29 @@
 require "language/go"
 
 class Aurl < Formula
-  homepage "https://github.com/classmethod-aws/aurl"
-  url "https://github.com/classmethod-aws/aurl.git", :tag => "0.4", :revision => "8fbff6ef9b4113dae2082c2e7bed0ae8a26c2f31"
+  homepage "https://github.com/classmethod/aurl"
+  url "https://github.com/classmethod/aurl.git", :tag => "1.0", :revision => "f5bc6d2a4c3df3b40f5f172d89388db903aeacb2"
 
-  head "https://github.com/classmethod-aws/aurl.git", :branch => "develop"
+  head "https://github.com/classmethod/aurl.git", :branch => "develop"
 
   depends_on "go" => :build
-
-  go_resource "github.com/codegangsta/cli" do
-    url "https://github.com/codegangsta/cli.git", :revision => "9b2bd2b3489748d4d0a204fa4eb2ee9e89e0ebc6"
-  end
-
-  go_resource "github.com/grategames/ini" do
-    url "https://github.com/grategames/ini.git", :revision => "860b5140bc02fe9438df6526fed2348b2b3f192a"
-  end
-
-  go_resource "github.com/rakyll/goini" do
-    url "https://github.com/rakyll/goini.git", :revision => "907cca0f578a5316fb864ec6992dc3d9730ec58c"
-  end
-
-  go_resource "github.com/mitchellh/go-homedir" do
-    url "https://github.com/mitchellh/go-homedir.git", :revision => "1f6da4a72e57d4e7edd4a7295a585e0a3999a2d4"
-  end
-
-  go_resource "golang.org/x/net" do
-    url "https://go.googlesource.com/net.git", :revision => "7dbad50ab5b31073856416cdcfeb2796d682f844"
-  end
-
-  go_resource "golang.org/x/oauth2" do
-    url "https://go.googlesource.com/oauth2.git", :revision => "11c60b6f71a6ad48ed6f93c65fa4c6f9b1b5b46a"
-  end
+  depends_on "glide" => :build
 
   def install
-    ENV["GOPATH"] = buildpath
-    mkdir_p buildpath/"src/github.com/classmethod-aws/"
-    ln_sf buildpath, buildpath/"src/github.com/classmethod-aws/aurl"
-    Language::Go.stage_deps resources, buildpath/"src"
+    (buildpath + "src/github.com/classmethod/aurl").install Dir[buildpath/"*"]
 
-    # Build and install
-    system "go", "build", "-o", "aurl"
-    bin.install "aurl"
+    cd "src/github.com/classmethod/aurl" do
+      ENV["GOPATH"] = buildpath
+      system "glide", "install"
+      system "go", "build", "-o", "aurl"
+    end
+    
+    bin.install "src/github.com/classmethod/aurl/aurl"
   end
 
   test do
     ENV["LC_ALL"] = "en_US.UTF-8"
-    system "#{bin}/aurl", "-v"
+    system "#{bin}/aurl", "-V"
   end
 
   def caveats
@@ -60,7 +38,7 @@ class Aurl < Formula
   \__\/: . . |  | ' |  | ||  | '   '  : |__
   ," .--.; | :  | : ;  ; |;  : |   |  | '.'|
  /  /  ,.  | '  :  `--'   \  , ;   ;  :    ;
-;  :   .'   \:  ,      .-./---'    |  ,   /
+;  :   .'   \:  ,      .-./---'    |  ,   /  v1.0
 |  ,     .-./ `--`----'             ---`-'
  `--`---'
 
